37d0d2d52ef716e60b84982c724b34ecb8851a66
Shoma Tanaka <tanashou0210@gmail.com>
2022-10-17 19:06:56 +0900

    Initial Commit

---
+++ InfinitePageSwiftUI/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  InfinitePageSwiftUI
+//
+//  Created by 田中翔真 on 2022/10/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ InfinitePageSwiftUI/InfinitePageSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  InfinitePageSwiftUIApp.swift
+//  InfinitePageSwiftUI
+//
+//  Created by 田中翔真 on 2022/10/17.
+//
+
+import SwiftUI
+
+@main
+struct InfinitePageSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
